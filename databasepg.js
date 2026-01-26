657e8bc9cd5b83cefe0850600b125fefe0271545
olivier Rukundo <rr.olivier01@gmail.com>
2026-01-26 22:33:11 +0100

    added basic SELECT query to fetch all actors from database

--- databasepg.js
+++ databasepg.js
@@ -24,3 +24,38 @@ client.query('SELECT * FROM actor', (err, res)=>{
     }
     client.end;
 })
+
+
+
+// 2. Insert a new actor
+const insertQuery = "INSERT INTO actor (first_name, last_name) VALUES ($1, $2) RETURNING *";
+const values = ['John', 'Doe'];
+client.query(insertQuery, values, (err, res) => {
+    if (!err) {
+        console.log("Inserted actor:", res.rows[0]);
+    } else {
+        console.log(err.message);
+    }
+});
+
+// 3. Update an actor
+const updateQuery = "UPDATE actor SET first_name=$1 WHERE actor_id=$2 RETURNING *";
+const updateValues = ['Johnny', 1]; // update actor with ID 1
+client.query(updateQuery, updateValues, (err, res) => {
+    if (!err) {
+        console.log("Updated actor:", res.rows[0]);
+    } else {
+        console.log(err.message);
+    }
+});
+
+// 4. Delete an actor
+const deleteQuery = "DELETE FROM actor WHERE actor_id=$1 RETURNING *";
+const deleteValue = [2]; // delete actor with ID 2
+client.query(deleteQuery, deleteValue, (err, res) => {
+    if (!err) {
+        console.log("Deleted actor:", res.rows[0]);
+    } else {
+        console.log(err.message);
+    }
+});
